e03c580d6a75c0a8428b8b92a8fd145abc5a0493
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2018-10-24 04:00:54 +0900

    Initial

---
+++ fetcher.ts
@@ -0,0 +1,94 @@
+import axios from 'axios';
+import { JSDOM } from 'jsdom';
+import { first, map, tail, trim } from 'lodash';
+import * as moment from 'moment';
+import * as striptags from 'striptags';
+
+const BASE_URL = 'http://www.keyakizaka46.com';
+
+const toURL = (path: string): string => {
+  return `${BASE_URL}${path}`;
+};
+
+export interface Schedule {
+  title?: string;
+  time: ScheduleTime;
+  genre?: string;
+  description?: string;
+}
+
+export interface ScheduleTime {
+  from?: string;
+  to?: string;
+}
+
+const normalize = (str: string | undefined | null): string | undefined => {
+  return str ? trim(striptags(str)) || undefined : undefined;
+};
+
+export const parseTime = (time?: string): ScheduleTime => {
+  if (time) {
+    const [from, to] = time.split('～');
+    return { from: normalize(from), to: normalize(to) };
+  } else {
+    return { from: undefined, to: undefined };
+  }
+};
+
+const parseScheduleElement = (element: Element): Schedule | undefined => {
+  const titleDescriptionElement = element.querySelector(
+    '.box-detail_txt :last-child',
+  );
+  const genreElement = element.querySelector('.box-detail_genre');
+  const timeElement = element.querySelector('.box-detail_txt .time');
+
+  if (titleDescriptionElement) {
+    const titleDescriptions = trim(
+      striptags(titleDescriptionElement.innerHTML),
+    ).split(/\n/);
+    const title = first(titleDescriptions);
+    const description = normalize(tail(titleDescriptions).join('\n'));
+    const time = parseTime(
+      timeElement ? normalize(timeElement.innerHTML) : undefined,
+    );
+
+    return {
+      title,
+      genre: genreElement ? normalize(genreElement.innerHTML) : undefined,
+      time,
+      description,
+    };
+  } else {
+    return undefined;
+  }
+};
+
+export const fetchSchedules = async (
+  date: moment.Moment,
+): Promise<Schedule[]> => {
+  return new Promise<Schedule[]>(async (resolve, reject) => {
+    try {
+      const res = await axios.get(
+        toURL(`/s/k46o/media/list?dy=${date.format('YYYYMMDD')}`),
+      );
+      const scheduleElements = new JSDOM(
+        res.data,
+      ).window.document.querySelectorAll(
+        '#schedule .box-schedule_inner .box-detail',
+      );
+      const schedules = map(scheduleElements, element =>
+        parseScheduleElement(element),
+      ).filter(schedule => schedule) as Schedule[];
+
+      resolve(schedules);
+    } catch (err) {
+      reject(err);
+    }
+  });
+};
+
+export const toValidJSON = (js: string): string => {
+  return js
+    .replace(/([a-zA-Z]+):/g, '"$1":')
+    .replace(/'(.*)'(,?)/g, (_, p1, p2) => `${JSON.stringify(p1)}${p2}`);
+};
---
+++ handler.ts
@@ -0,0 +1,61 @@
+import { Callback, Context, Handler, ScheduledEvent } from 'aws-lambda';
+import { fetchSchedules } from './fetcher';
+import * as moment from 'moment';
+import { WebClient } from '@slack/client';
+
+export const notify: Handler = async (
+  event: ScheduledEvent,
+  context: Context,
+  cb: Callback,
+) => {
+  try {
+    if (typeof process.env.SLACK_TOKEN === 'undefined') {
+      throw new Error('SLACK_TOKEN is not set');
+    }
+
+    const slackToken = process.env.SLACK_TOKEN;
+
+    if (typeof process.env.SLACK_CHANNEL === 'undefined') {
+      throw new Error('SLACK_CHANNEL is not set');
+    }
+
+    const channel = process.env.SLACK_CHANNEL;
+    const slack = new WebClient(slackToken);
+    const schedules = await fetchSchedules(moment());
+    console.log(schedules);
+    schedules.forEach(schedule => {
+      const fields: { title: string; value: string; short?: boolean }[] = [];
+      if (schedule.time.from || schedule.time.to) {
+        fields.push({
+          title: 'Time',
+          value: `${schedule.time.from || ''}~${schedule.time.to || ''}`,
+          short: true,
+        });
+      }
+      if (schedule.genre) {
+        fields.push({
+          title: 'Genre',
+          value: schedule.genre,
+          short: true,
+        });
+      }
+
+      slack.chat.postMessage({
+        channel: channel,
+        text: schedule.title || '',
+        attachments: [
+          {
+            fields,
+          },
+        ],
+        username: '今日のスケジュール',
+        icon_url: 'http://www.keyakizaka46.com/files/14/images/top/logo_l.jpg',
+        mrkdwn: false,
+      });
+    });
+    console.log('Finished');
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+};
