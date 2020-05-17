22cdd83fdb40871b2c3222ec3886facaf69a8680
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2020-05-17 10:45:16 +0900

    Remove canvas

--- webpack.config.js
+++ webpack.config.js
@@ -25,8 +25,5 @@ module.exports = {
       // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
       { test: /\.tsx?$/, loader: 'ts-loader' },
     ],
-  },
-  externals: {
-    canvas: {},
-  },
+  }
 };
