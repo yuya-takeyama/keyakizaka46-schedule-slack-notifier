62fb49f9b77b9f2a32f2815b166a43f2c00f5d85
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2020-09-27 23:16:36 +0900

    Fix loading canvas in jsdom

---
+++ src/functions/canvas/index.js
@@ -0,0 +1,2 @@
+// Workaround for loading canvas in jsdom
+// ref: https://qiita.com/h1romas4/items/5048f68e16cadc4e6fb8#lambda-%E3%81%A7-jsdom-%E3%82%92%E4%BD%BF%E3%81%A3%E3%81%A6-dom-%E3%82%92%E6%93%8D%E4%BD%9C%E3%81%99%E3%82%8B
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,13 @@
 const path = require('path');
 const slsw = require('serverless-webpack');
-const nodeExternals = require('webpack-node-externals');
-
-const entries = {};
-
-Object.keys(slsw.lib.entries).forEach(
-  key => (entries[key] = ['./source-map-install.js', slsw.lib.entries[key]]),
-);
 
 module.exports = {
   mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
-  entry: entries,
+  entry: slsw.lib.entries,
   devtool: 'source-map',
   resolve: {
     extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
   },
-  externals: [nodeExternals()],
   output: {
     libraryTarget: 'commonjs',
     path: path.join(__dirname, '.webpack'),
